3fb8783c8046bf9e952c80f11cbbaa307a487dae
jensstraffr <67412389+jensstraffr@users.noreply.github.com>
2023-08-09 13:48:18 +0200

    Small improvements

--- app/src/main/java/com/example/locationtest/MainActivity.kt
+++ app/src/main/java/com/example/locationtest/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.location.*
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
+
 //import kotlin.random.Random
 
 data class LapTimeItem(val lap: String, val lapTime: Long)
@@ -34,118 +35,27 @@ class MainActivity : AppCompatActivity() {
             setGranularity(Granularity.GRANULARITY_PERMISSION_LEVEL)
             setWaitForAccurateLocation(true)
         }.build()
-    private val maxLaps:Int=15
-    private lateinit var tvRoundNo:TextView
-    private lateinit var tvRoundDescription:TextView
+    private val maxLaps: Int = 15
+    private lateinit var tvRoundNo: TextView
+    private lateinit var tvRoundDescription: TextView
     private lateinit var tvLaptime: TextView
     private lateinit var tvTimediff: TextView
     private lateinit var tvWaypoint: TextView
     private lateinit var tvSpeed: TextView
     private lateinit var tvTime: TextView
-    private lateinit var rvLaps:RecyclerView
-    private var time:Long = 0
+    private lateinit var rvLaps: RecyclerView
+    private var time: Long = 0
     private var timeRemember = System.currentTimeMillis()
     private var lastWptIndex: Int = -1
-    private var lapNo:Int=0
-    private val confirmationLap:Int=9
-    private lateinit var lapTimes:LongArray
-    private lateinit var bestTimes:LongArray
-    private lateinit var sectorTimes:Array<Array<Long>>
+    private var lapNo: Int = 0
+    private val confirmationLap: Int = 9
+    private lateinit var lapTimes: LongArray
+    private lateinit var bestTimes: LongArray
+    private lateinit var sectorTimes: Array<Array<Long>>
     private var lapTimeItems = mutableListOf<LapTimeItem>()
     private val scheduledExecutor = Executors.newSingleThreadScheduledExecutor()
     private val delayMillis = 100L // 0,1 second
     private val initialDelayMillis = 0L // Start the task immediately
-    /*private val handler2 = Handler(Looper.getMainLooper())
-    private var runnable: Runnable? = null
-    private data class Waypoint(
-        val name: String,
-        val latitude: Double,
-        val longitude: Double,
-    )
-    private data class Testlocation(
-        val latitude:Double,
-        val longitude: Double,
-        val speed:Double
-    )
-    private val waypoints = arrayOf(
-        Waypoint("Vorstart",50.0836,8.54),
-        Waypoint("Start/Ziel",51.08337,9.54105),
-        Waypoint("Vorstart",50.0836,8.54),
-        Waypoint("BiÖ1",51.08275,9.54072),
-        Waypoint("Vorstart",50.0836,8.54),
-        Waypoint("Kurve2",51.08239,9.54191),
-        Waypoint("Vorstart",50.0836,8.54),
-        Waypoint("Kurve3",51.08326,9.54332),
-        Waypoint("Vorstart",50.0836,8.54),
-        Waypoint("BiÖ11",51.08395,9.54159)
-    )
-    private var wptPosition=0
-    private fun testCallback(location:Testlocation) {
-        if (lapNo<=maxLaps) {
-            tvSpeed.text = String.format("%.2f", (location.speed * 3.6))
-            val wpt = Nordschleife.isWaypointReached(location.latitude, location.longitude)
-            if (wpt.waypointIndex != lastWptIndex && wpt.waypointIndex != -1) {
-                lastWptIndex = wpt.waypointIndex
-                if (wpt.waypointIndex == 0) {
-                    lapNo++
-                    if (lapNo == 1) {
-                        showSeconds(true)
-                    } else {
-                        time = System.currentTimeMillis() - timeRemember
-                        lapTimes[lapNo - 2] = time
-                        var newItem = LapTimeItem(getString(R.string.round, lapNo - 1), time)
-                        lapTimeItems.add(newItem)
-                        rvLaps.adapter!!.notifyItemInserted(lapTimeItems.size - 1)
-                        rvLaps.scrollToPosition(lapTimeItems.size - 1)
-                        if (lapNo==9) {
-                            newItem= LapTimeItem(getString(R.string.r7_r8_combined),
-                                lapTimes[6]+lapTimes[7])
-                            lapTimeItems.add(newItem)
-                            rvLaps.adapter!!.notifyItemInserted(lapTimeItems.size - 1)
-                            rvLaps.scrollToPosition(lapTimeItems.size - 1)
-                        } else if (lapNo==10) {
-                            newItem= LapTimeItem(getString(R.string.diff_set_confirmation),
-                                lapTimes[0]-lapTimes[8])
-                            lapTimeItems.add(newItem)
-                            rvLaps.adapter!!.notifyItemInserted(lapTimeItems.size - 1)
-                            rvLaps.scrollToPosition(lapTimeItems.size - 1)
-                        }
-                    }
-                    if (lapNo > maxLaps) {
-                        tvRoundDescription.text = getString(R.string.end_of_race)
-                        handler2.removeCallbacks(runnable!!)
-                        showSeconds(false)
-                        fusedLocationProvider?.removeLocationUpdates(locationCallback)
-                        return
-                    }
-                    tvRoundNo.text = getString(R.string.round, lapNo)
-                    tvRoundDescription.text = Nordschleife.getRoundName(lapNo)
-                    timeRemember = System.currentTimeMillis()
-                } else {
-                    time = System.currentTimeMillis() - timeRemember
-                }
-                tvWaypoint.text = wpt.waypointName
-                tvTime.text = formatMillisecondsToTime(time)
-                sectorTimes[lapNo - 1][lastWptIndex] = time
-                var timediff = time - bestTimes[lastWptIndex]
-                if (lapNo == confirmationLap) {
-                    timediff = time - sectorTimes[0][lastWptIndex]
-                }
-                if (time < bestTimes[lastWptIndex] || bestTimes[lastWptIndex] == 0L) bestTimes[lastWptIndex] =
-                    time
-                if (timediff <= 0) {
-                    tvTimediff.setTextColor(getColor(R.color.green))
-                } else {
-                    tvTimediff.setTextColor(getColor(R.color.red))
-                }
-                if (lapNo == 1) {
-                    tvTimediff.text = ""
-                } else {
-                    tvTimediff.text = String.format("%.2f", (timediff.toDouble() / 1000))
-                }
-            }
-        }
-    }*/
     private var locationCallback: LocationCallback = object : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult) {
             val locationList = locationResult.locations
@@ -189,7 +99,6 @@ class MainActivity : AppCompatActivity() {
                             }
                             if (lapNo > maxLaps) {
                                 tvRoundDescription.text = getString(R.string.end_of_race)
-                                //handler2.removeCallbacks(runnable!!)
                                 showSeconds(false)
                                 stopLocationUpdates()
                                 return
@@ -201,24 +110,30 @@ class MainActivity : AppCompatActivity() {
                             time = System.currentTimeMillis() - timeRemember
                         }
                         tvWaypoint.text = wpt.waypointName
-                        tvTime.text = formatMillisecondsToTime(time)
-                        sectorTimes[lapNo - 1][lastWptIndex] = time
-                        var timediff = time - bestTimes[lastWptIndex]
-                        if (lapNo == confirmationLap) {
-                            timediff = time - sectorTimes[0][lastWptIndex]
+                        if (lapNo>0) {
+                            tvTime.text = formatMillisecondsToTime(time)
+                            sectorTimes[lapNo - 1][lastWptIndex] = time
+                            var timediff = time - bestTimes[lastWptIndex]
+                            if (lapNo == confirmationLap) {
+                                timediff = time - sectorTimes[0][lastWptIndex]
+                            }
+                            if (time < bestTimes[lastWptIndex] || bestTimes[lastWptIndex] == 0L)
+                                bestTimes[lastWptIndex] = time
+                            if (timediff <= 0) {
+                                tvTimediff.setTextColor(getColor(R.color.green))
+                            } else {
+                                tvTimediff.setTextColor(getColor(R.color.red))
+                            }
+                            if (lapNo == 1 || (lapNo == 2 && wpt.waypointIndex == 0)) {
+                                tvTimediff.text = ""
+                            } else {
+                                tvTimediff.text =
+                                    String.format("%.2f", (timediff.toDouble() / 1000))
+                            }
                         }
-                        if (time < bestTimes[lastWptIndex] || bestTimes[lastWptIndex] == 0L) bestTimes[lastWptIndex] =
-                            time
-                        if (timediff <= 0) {
-                            tvTimediff.setTextColor(getColor(R.color.green))
-                        } else {
-                            tvTimediff.setTextColor(getColor(R.color.red))
-                        }
-                        if (lapNo == 1) {
-                            tvTimediff.text = ""
-                        } else {
-                            tvTimediff.text = String.format("%.2f", (timediff.toDouble() / 1000))
-                        }
+                    }
+                    if (lapNo==0) {
+                        tvRoundDescription.text=getString(R.string.prestart)
                     }
                 }
             }
@@ -231,36 +146,25 @@ class MainActivity : AppCompatActivity() {
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         fusedLocationProvider = LocationServices.getFusedLocationProviderClient(this)
         checkLocationPermission()
-        lapTimes= LongArray(maxLaps)
-        sectorTimes= Array(maxLaps) { Array(Nordschleife.getSectorCount()) {0} }
-        tvRoundNo=findViewById(R.id.tv_round_no)
-        tvRoundDescription=findViewById(R.id.tv_round_description)
+        lapTimes = LongArray(maxLaps)
+        sectorTimes = Array(maxLaps) { Array(Nordschleife.getSectorCount()) { 0 } }
+        tvRoundNo = findViewById(R.id.tv_round_no)
+        tvRoundDescription = findViewById(R.id.tv_round_description)
         tvLaptime = findViewById(R.id.tv_laptime)
         tvTimediff = findViewById(R.id.tv_timediff)
         tvWaypoint = findViewById(R.id.tv_waypoint)
         tvSpeed = findViewById(R.id.tv_speed)
         tvTime = findViewById(R.id.tv_time)
-        tvWaypoint.text=getString(R.string.prestart)
-        rvLaps=findViewById(R.id.rv_rundenliste)
-        rvLaps.layoutManager=LinearLayoutManager(this)
-        bestTimes= LongArray(Nordschleife.getSectorCount()) {0L}
+        tvWaypoint.text = getString(R.string.prestart)
+        rvLaps = findViewById(R.id.rv_rundenliste)
+        rvLaps.layoutManager = LinearLayoutManager(this)
+        bestTimes = LongArray(Nordschleife.getSectorCount()) { 0L }
         val adapter = ItemAdapter(lapTimeItems)
         rvLaps.adapter = adapter
         rvLaps.layoutManager = LinearLayoutManager(this)
-        /*runnable = object : Runnable {
-            override fun run() {
-                val waypoint = waypoints[wptPosition++]
-                if (wptPosition > waypoints.size - 1) wptPosition = 0
-                val random = Random.Default
-                val location = Testlocation(waypoint.latitude, waypoint.longitude, random.nextDouble(5.0, 50.0))
-                testCallback(location)
-                handler2.postDelayed(this, random.nextLong(800, 1200)) // Schedule the task again after the delay
-            }
-        }
-
-        handler2.postDelayed(runnable as Runnable, 1000)*/
     }
-    private fun showSeconds(doShow:Boolean) {
+
+    private fun showSeconds(doShow: Boolean) {
         if (doShow) {
             scheduledExecutor.scheduleAtFixedRate({
                 runOnUiThread {
@@ -272,15 +176,16 @@ class MainActivity : AppCompatActivity() {
             scheduledExecutor.shutdown()
         }
     }
+
     private fun stopLocationUpdates() {
         fusedLocationProvider?.removeLocationUpdates(locationCallback)
     }
+
     override fun onResume() {
         super.onResume()
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
             == PackageManager.PERMISSION_GRANTED
         ) {
-
             fusedLocationProvider?.requestLocationUpdates(
                 locationRequest,
                 locationCallback,
@@ -295,13 +200,10 @@ class MainActivity : AppCompatActivity() {
         if (ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
-            )
-            == PackageManager.PERMISSION_GRANTED
+            ) == PackageManager.PERMISSION_GRANTED
         ) {
-
             fusedLocationProvider?.removeLocationUpdates(locationCallback)
         }
-        //scheduledExecutor.shutdown()
     }
 
     private fun checkLocationPermission() {
@@ -316,9 +218,6 @@ class MainActivity : AppCompatActivity() {
                     Manifest.permission.ACCESS_FINE_LOCATION
                 )
             ) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
                 AlertDialog.Builder(this)
                     .setTitle("Location Permission Needed")
                     .setMessage("This app needs the Location permission, please accept to use location functionality")
@@ -400,19 +299,10 @@ class MainActivity : AppCompatActivity() {
                             locationCallback,
                             Looper.getMainLooper()
                         )
-
-                        // Now check background location
                         checkBackgroundLocation()
                     }
 
                 } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    //Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show()
-
-                    // Check if we are in a state where the user has denied the permission and
-                    // selected Don't ask again
                     if (!ActivityCompat.shouldShowRequestPermissionRationale(
                             this,
                             Manifest.permission.ACCESS_FINE_LOCATION
@@ -432,9 +322,6 @@ class MainActivity : AppCompatActivity() {
             MY_PERMISSIONS_REQUEST_BACKGROUND_LOCATION -> {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // location-related task you need to do.
                     if (ContextCompat.checkSelfPermission(
                             this,
                             Manifest.permission.ACCESS_FINE_LOCATION
@@ -452,32 +339,29 @@ class MainActivity : AppCompatActivity() {
                             Toast.LENGTH_LONG
                         ).show()
                     }
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    //Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show()
                 }
                 return
 
             }
         }
     }
+
     @Suppress("UNUSED_PARAMETER")
-    fun gotoStartpage(v:View) {
+    fun gotoStartpage(v: View) {
         stopLocationUpdates()
         showSeconds(false)
         val intent = Intent(this, StartActivity::class.java)
         startActivity(intent)
         finish()
     }
+
     @Suppress("UNUSED_PARAMETER")
-    fun restart(v:View) {
-        lapTimes= LongArray(maxLaps)
-        sectorTimes= Array(maxLaps) { Array(Nordschleife.getSectorCount()) {0} }
-        lapNo=0
+    fun restart(v: View) {
+        lapTimes = LongArray(maxLaps)
+        sectorTimes = Array(maxLaps) { Array(Nordschleife.getSectorCount()) { 0 } }
+        lapNo = 0
         showSeconds(false)
-        lastWptIndex=-1
+        lastWptIndex = -1
         rvLaps.adapter?.apply {
             val itemCount = itemCount // Get the current item count
             if (itemCount > 0) {
@@ -486,14 +370,16 @@ class MainActivity : AppCompatActivity() {
             }
         }
         setContentView(R.layout.activity_main)
+        tvWaypoint.text = getString(R.string.prestart)
     }
+
     fun formatMillisecondsToTime(milliseconds: Long): String {
         val totalSeconds = milliseconds / 1000
         //val hours = totalSeconds / 3600
         val minutes = (totalSeconds % 3600) / 60
         val seconds = totalSeconds % 60
         val millis = (milliseconds % 1000) / 10
-        return String.format("%02d:%02d.%02d",  minutes, seconds, millis)
+        return String.format("%02d:%02d.%02d", minutes, seconds, millis)
     }
 
     companion object {
--- app/src/main/java/com/example/locationtest/Nordschleife.kt
+++ app/src/main/java/com/example/locationtest/Nordschleife.kt
@@ -10,63 +10,64 @@ class Nordschleife {
             val latitude2: Double,
             val longitude2: Double
         )
+
         data class WpData(
             val waypointName: String,
-            val waypointIndex:Int
+            val waypointIndex: Int
         )
-        private val rounds= listOf<String>(
-            "Setzrunde","Sprint","Sprint","Sprint",
-            "Sprint", "Sprint", "InLap","OutLap","Bestätigungsrunde","Sprint","Sprint",
+
+        private val rounds = listOf<String>(
+            "Setzrunde", "Sprint", "Sprint", "Sprint",
+            "Sprint", "Sprint", "InLap", "OutLap", "Bestätigungsrunde", "Sprint", "Sprint",
             "Sprint", "Sprint", "Sprint", "InLap MaxZeit"
         )
         private val waypoints = listOf(
-            Waypoint("Start/Ziel",51.08307,9.54045,51.08328,9.54026),
-            Waypoint("Fuchsröhre",51.08329,9.5393,51.08349,9.53968),
-            Waypoint("Bergwerk",51.08411,9.53971,51.08392,9.53995),
-            Waypoint("Brünnchen",51.08432,9.54101,51.08457,9.54076),
-            Waypoint("Döttinger Höhe",51.08366,9.54129,51.08393,9.54113)
-//    Waypoint("Hocheichen",50.33963,6.93437,50.34188,6.93346),
-//    Waypoint("Flugplatz",50.34483,6.92614,50.34621,6.9266),
-//    Waypoint("Schwedenkreuz",50.35467,6.92385,50.35661,6.92434),
-//    Waypoint("Fuchsröhre",50.35851,6.92295,50.35993,6.9234),
-//    Waypoint("Adenauer Forst",50.36478,6.93089,50.36675,6.93074),
-//    Waypoint("Metzgesfeld",50.37203,6.93694,50.37054,6.93844),
-//    Waypoint("Kallenhard",50.37341,6.93443,50.37502,6.93355),
-//    Waypoint("Miss-Hit-Miss",50.37696,6.93943,50.37835,6.93754),
-//    Waypoint("Wehrseifen",50.37557,6.94368,50.3774,6.94203),
-//    Waypoint("Ex-Mühle",50.37743,6.95124,50.37882,6.94963),
-//    Waypoint("Bergwerk",50.38023,6.96107,50.38135,6.95933),
-//    Waypoint("Kesselchen",50.37423,6.96566,50.37519,6.96821),
-//    Waypoint("Mutkurve",50.37197,6.97843,50.37318,6.9815),
-//    Waypoint("Steilstrecke",50.37405,6.98955,50.37493,6.98783),
-//    Waypoint("Hohe Acht",50.37491,6.99453,50.37636,6.99339),
-//    Waypoint("Wippermann",50.37498,6.99966,50.37551,7.00279),
-//    Waypoint("Eschbach",50.37148,7.00402,50.37275,7.00236),
-//    Waypoint("Brünnchen",50.36831,7.00503,50.36914,7.00354),
-//    Waypoint("Pflanzgarten 1",50.36483,7.0003,50.36593,6.99904),
-//    Waypoint("Pflanzgarten 2",50.36039,6.99534,50.36219,6.99485),
-//    Waypoint("Schwalbenschwanz",50.35754,6.98719,50.35888,6.98539),
-//    Waypoint("Galgenkopf",50.35553,6.98405,50.35651,6.98644),
-//    Waypoint("Döttinger Höhe",50.35196,6.98227,50.35193,6.98026),
-//    Waypoint("Touristenzufahrt",50.34632,6.96729,50.34625,6.96525),
-//    Waypoint("Antoniusbuche",50.34361,6.96072,50.34316,6.95825),
-//    Waypoint("Hohenrain",50.33842,6.95379,50.33801,6.95261)
+            Waypoint("Start/Ziel", 50.33754, 6.95043, 50.33859, 6.95012),
+            Waypoint("Hocheichen", 50.33963, 6.93437, 50.34188, 6.93346),
+            Waypoint("Flugplatz", 50.34483, 6.92614, 50.34621, 6.9266),
+            Waypoint("Schwedenkreuz", 50.35467, 6.92385, 50.35661, 6.92434),
+            Waypoint("Fuchsröhre", 50.35851, 6.92295, 50.35993, 6.9234),
+            Waypoint("Adenauer Forst", 50.36478, 6.93089, 50.36675, 6.93074),
+            Waypoint("Metzgesfeld", 50.37203, 6.93694, 50.37054, 6.93844),
+            Waypoint("Kallenhard", 50.37341, 6.93443, 50.37502, 6.93355),
+            Waypoint("Miss-Hit-Miss", 50.37696, 6.93943, 50.37835, 6.93754),
+            Waypoint("Wehrseifen", 50.37557, 6.94368, 50.3774, 6.94203),
+            Waypoint("Ex-Mühle", 50.37743, 6.95124, 50.37882, 6.94963),
+            Waypoint("Bergwerk", 50.38023, 6.96107, 50.38135, 6.95933),
+            Waypoint("Kesselchen", 50.37423, 6.96566, 50.37519, 6.96821),
+            Waypoint("Mutkurve", 50.37197, 6.97843, 50.37318, 6.9815),
+            Waypoint("Steilstrecke", 50.37405, 6.98955, 50.37493, 6.98783),
+            Waypoint("Hohe Acht", 50.37491, 6.99453, 50.37636, 6.99339),
+            Waypoint("Wippermann", 50.37498, 6.99966, 50.37551, 7.00279),
+            Waypoint("Eschbach", 50.37148, 7.00402, 50.37275, 7.00236),
+            Waypoint("Brünnchen", 50.36831, 7.00503, 50.36914, 7.00354),
+            Waypoint("Pflanzgarten 1", 50.36483, 7.0003, 50.36593, 6.99904),
+            Waypoint("Pflanzgarten 2", 50.36039, 6.99534, 50.36219, 6.99485),
+            Waypoint("Schwalbenschwanz", 50.35754, 6.98719, 50.35888, 6.98539),
+            Waypoint("Galgenkopf", 50.35553, 6.98405, 50.35651, 6.98644),
+            Waypoint("Döttinger Höhe", 50.35196, 6.98227, 50.35193, 6.98026),
+            Waypoint("Touristenzufahrt", 50.34632, 6.96729, 50.34625, 6.96525),
+            Waypoint("Antoniusbuche", 50.34361, 6.96072, 50.34316, 6.95825),
+            Waypoint("Hohenrain", 50.33842, 6.95379, 50.33801, 6.95261)
         )
 
-        fun isWaypointReached(latitude:Double,longitude:Double): WpData {
-            for ((index,waypoint) in waypoints.withIndex()) {
+        fun isWaypointReached(latitude: Double, longitude: Double): WpData {
+            for ((index, waypoint) in waypoints.withIndex()) {
                 if (latitude >= minOf(waypoint.latitude1, waypoint.latitude2)
                     && latitude <= maxOf(waypoint.latitude1, waypoint.latitude2)
                     && longitude >= minOf(waypoint.longitude1, waypoint.longitude2)
-                    && longitude <= maxOf(waypoint.longitude1, waypoint.longitude2))
-                    return WpData(waypoint.name,index)
+                    && longitude <= maxOf(waypoint.longitude1, waypoint.longitude2)
+                )
+                    return WpData(waypoint.name, index)
             }
-            return WpData("",-1)
+            return WpData("", -1)
         }
-        fun getRoundName(roundNo:Int):String {
-            return rounds[roundNo-1]
+
+        fun getRoundName(roundNo: Int): String {
+            return rounds[roundNo - 1]
         }
-        fun getSectorCount():Int {
+
+        fun getSectorCount(): Int {
             return waypoints.count()
         }
     }
